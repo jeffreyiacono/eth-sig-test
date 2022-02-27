64c4673f262201dbc9b450ae921782df02237cfa
Jeff Iacono <jeff.iacono@gmail.com>
2022-02-27 09:46:08 -0800

    Refactor and add better descriptions

--- create-sigs.js
+++ create-sigs.js
@@ -1,46 +1,48 @@
 require("dotenv").config();
 const EthCrypto = require("eth-crypto");
 
-// const signerIdentity = EthCrypto.createIdentity();
-// console.log("signerIdentity:");
-// console.log(signerIdentity);
-//const message = EthCrypto.hash.keccak256("\x19Ethereum Signed Message:\n32", [
-//  { type: "string", value: "Hello World!" },
-//]);
-
 const contractAddress = "0x1a51053bD31cbbDbf0a5EAB4517E74435e9C397B";
 const payeeAddress = "0x39Cd295191EdF125b46a5e73593666f1b238B7e8";
 
-console.log("⚙️  Generating signatures for", contractAddress);
-const claimPaymentMessage = EthCrypto.hash.keccak256([
-  { type: "address", value: payeeAddress },
-  { type: "uint256", value: "1" },
-  { type: "uint256", value: "1" },
-  { type: "address", value: contractAddress },
-]);
-const claimPaymentSignature = EthCrypto.sign(
-  process.env.PRIVATE_KEY,
-  claimPaymentMessage
-);
+hashAndSign = (content, private_key) => {
+  const message = EthCrypto.hash.keccak256(content);
+  const signature = EthCrypto.sign(private_key, message);
+  return [message, signature];
+};
 
+console.log("⚙️  Generating signatures for contract @", contractAddress);
+// TODO: add the header + message len + message ...
+//const message = EthCrypto.hash.keccak256("\x19Ethereum Signed Message:\n32"...
+
+console.log("\n");
+console.log("🏦 Checking account (ie. claim payment) example ...");
+const [claimPaymentMessage, claimPaymentSignature] = hashAndSign(
+  [
+    { type: "address", value: payeeAddress },
+    { type: "uint256", value: "1" },
+    { type: "uint256", value: "1" },
+    { type: "address", value: contractAddress },
+  ],
+  process.env.PRIVATE_KEY
+);
 console.log(
-  ` - 📝  Hashed claimPaymentMessage that can be used with sig to verify signer: ${claimPaymentMessage}`
+  ` - 📝 Hashed claimPaymentMessage that can be used with sig to verify signer: ${claimPaymentMessage}`
 );
 console.log(
   ` - ✍️  Signature to use for claimPayment: ${claimPaymentSignature}`
 );
 
-const claimCodeMessage = EthCrypto.hash.keccak256([
-  { type: "string", value: "dan tong #1" },
-  { type: "address", value: contractAddress },
-]);
-const claimCodeSignature = EthCrypto.sign(
-  process.env.PRIVATE_KEY,
-  claimCodeMessage
+console.log("\n");
+console.log("🕵️‍♀️  Claim code example ...");
+const [claimCodeMessage, claimCodeSignature] = hashAndSign(
+  [
+    { type: "string", value: "dan tong #1" },
+    { type: "address", value: contractAddress },
+  ],
+  process.env.PRIVATE_KEY
 );
-
 console.log(
-  ` - 📝  Hashed claimCodeMessage that can be used with sig to verify signer: ${claimCodeMessage}`
+  ` - 📝 Hashed claimCodeMessage that can be used with sig to verify signer: ${claimCodeMessage}`
 );
 console.log(
   ` - ✍️  Signature to use for claimCodeSignature: ${claimCodeSignature}`

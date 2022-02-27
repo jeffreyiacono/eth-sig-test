958643a4b25ce6d835ccec3a9dde560740ba1829
Jeff Iacono <jeff.iacono@gmail.com>
2022-02-27 14:20:09 -0800

    Adds ch example

--- create-sigs.js
+++ create-sigs.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const EthCrypto = require("eth-crypto");
 
 const contractAddress = "0x92c8ddd9CE4C4b37fB2165a2B31ad6c2E1C8B34e";
-const payeeAddress = "0x8Cd317Bcd6c79f3993dd25F971B3C728Fa0c1Cf5"; // dt's address
+const payeeAddress = "0x5e2eBd311Ed77dC6638c9f1998B8546C068067E5"; // ch's address
 
 hashAndSign = (content, private_key) => {
   const message = EthCrypto.hash.keccak256(content);
@@ -36,7 +36,7 @@ console.log("\n");
 console.log("🕵️‍♀️  Claim code example ...");
 const [claimCodeMessage, claimCodeSignature] = hashAndSign(
   [
-    { type: "string", value: "dan tong #1" },
+    { type: "string", value: "the meg #1" },
     { type: "address", value: contractAddress },
   ],
   process.env.PRIVATE_KEY
